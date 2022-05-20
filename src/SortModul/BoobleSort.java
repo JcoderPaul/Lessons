19d7a394204461c057a68f02bf9d66980a30717b
Paul <Jcoder.Paul@gmail.com>
2022-05-20 20:10:00 +0300

    Moving files to a new directory

--- src/SortModul/BoobleSort.java
+++ src/SortModul/BoobleSort.java
@@ -1,3 +1,5 @@
+package SortModul;
+
 public class BoobleSort
 {
     public static void main(String[] args) {
--- src/SortModul/ChoiceSorted.java
+++ src/SortModul/ChoiceSorted.java
@@ -1,3 +1,5 @@
+package SortModul;
+
 public class ChoiceSorted
 {
     public static void main(String[] args) {
